17506851251cf9bac29cde9a44c9f66983c1a771
wilzewski <jonas.wilzewski@posteo.de>
2022-10-24 22:55:03 -0400

    run on all osse data

--- test_osse.py
+++ test_osse.py
@@ -1,9 +1,11 @@
 from cloudFilter import cloudFilter
+import os
 
-l1 = '/scratch/sao_atmos/ahsouri/Retrieval/methanesat_osses/L1/Global_no_airglow/MethaneSAT_20210830.0_05h_l2prof_l1.nc_subset'
+l1 = '/scratch/sao_atmos/ahsouri/Retrieval/methanesat_osses/L1/Global_no_airglow/'
+#l1 = '/scratch/sao_atmos/ahsouri/Retrieval/methanesat_osses/L1/Global_no_airglow/MethaneSAT_20210830.0_02h_l2prof_l1.nc_subset'
 
 msi = '/scratch/sao_atmos/jwilzews/DATA/MSI/MSI_clim'
 
 fobj = cloudFilter(l1, msi, osse=True)
-
 fobj.get_prefilter()
+    
